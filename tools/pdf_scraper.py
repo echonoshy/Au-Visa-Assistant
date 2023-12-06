9d7546fada97e47005c57030fedf45200f60cab0
LAKE <1147359254@qq.com>
2023-12-06 17:21:49 +0800

    [feat] get pages links

--- tools/pdf_scraper.py
+++ tools/pdf_scraper.py
@@ -1,62 +1,71 @@
-# Scrape all visa-related documents from the immigration department's official website.
-
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import requests
 import time
 
 
+# Set up common Chrome options
+chrome_options = Options()
+# Uncomment the following lines if you want to run the script in headless mode
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--disable-gpu")
+
+
+# Function to get all visa-related page links
 def get_pages_link():
-    # 目标网页
     url = "https://immi.homeaffairs.gov.au/visas/getting-a-visa/visa-listing"
     index_url = "https://immi.homeaffairs.gov.au"
     pages_link = []
 
+    driver = webdriver.Chrome(options=chrome_options)
     try:
-        # 设置 Chrome 的 Options 以启用 Headless 模式
-        chrome_options = Options()
-        # chrome_options.add_argument("--headless")  # 确保无界面运行
-        # chrome_options.add_argument("--disable-gpu")  # 对于一些版本的Chrome，这个参数是必需的
-
-        # 初始化 WebDriver
-        driver = webdriver.Chrome(options=chrome_options)
-
-        # 打开网页
         driver.get(url)
-
-        # 等待页面加载（根据需要调整等待时间）
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, 'ha-wysiwyg > div > ul > li  a'))
         )
-
-        # 获取页面源码
         html_content = driver.page_source
-
-        # 使用 BeautifulSoup 解析 HTML
         soup = BeautifulSoup(html_content, 'html.parser')
 
-        # 遍历并打印链接
         for a_tag in soup.select('ha-wysiwyg > div > ul > li  a'):
             href = a_tag.get('href')
-            if not href.startswith(index_url):
+            if not href.startswith('http'):
                 href = index_url + href
-            # print(href)
             pages_link.append(href)
         return pages_link
-
     except Exception as e:
-        print(f"发生错误: {e}")
+        print(f"Error occurred: {e}")
         return []
     finally:
-        # 清理，关闭浏览器
         driver.quit()
 
+# Function to download PDF from a given visa page
+def get_pdf(url, pdf_name):
+    driver = webdriver.Chrome(options=chrome_options)
+    try:
+        driver.get(url)
+        print_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "ctl00_ctl00_NavigationTree_NavTreeFooterNode_ctl00_ctl00_NavigationTree_NavTreeFooterNode_FooterLegalLinks_HyperLinkPrint"))
+        )
+        print_button.click()
+        time.sleep(5)  # Adjust as necessary for page load
 
+        pdf_url = driver.current_url
+        response = requests.get(pdf_url)
+        if response.status_code == 200:
+            with open(pdf_name, 'wb') as f:
+                f.write(response.content)
+    except Exception as e:
+        print(f"Error occurred: {e}")
+    finally:
+        driver.quit()
 
 if __name__ == "__main__":
-    pages_link = get_pages_link()
-    print(pages_link)
+    # Example usage: get the first PDF link and download it
+    visa_links = get_pages_link()
+    if visa_links:
+        first_link = visa_links[0]
+        get_pdf(first_link, 'visa_document.pdf')
